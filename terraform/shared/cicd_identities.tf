c352d73517236fd0c769903c57c1a9b414af575d
Gerald Monticelli <gerald.monticelli@pm.me>
2026-09-15 12:53:55 +0200

    test job_workflow_ref for OIDC federation authentication on test workspace

--- terraform/shared/cicd_identities.tf
+++ terraform/shared/cicd_identities.tf
@@ -42,7 +42,7 @@ resource "databricks_service_principal_federation_policy" "github_cicd_test" {
   oidc_policy = {
     issuer = "https://token.actions.githubusercontent.com"
     subject_claim = "job_workflow_ref"
-    subject       = "GeraldMonticelli/northmart360/.github/workflows/workload-test.yml@refs/heads/main"
+    subject       = "GeraldMonticelli/northmart360/.github/workflows/oidc-auth-test.yml@refs/heads/test/databricks-oidc-auth"
     #subject = "repo:GeraldMonticelli/northmart360:environment:workload-test"
 
     audiences = [
